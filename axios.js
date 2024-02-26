e6cc609443edbc4ba93490488066cc88ddd00d33
Feev004 <feevfeev2@gmail.com>
2024-02-27 00:57:26 +0700

    up

--- axios.js
+++ axios.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const axios = require('axios');
 const app = express();
+const multer = require("multer")
 const path = require("path");
 
 //setproject
@@ -11,6 +12,16 @@ app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 
 const url = "http://localhost:3000";
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null,'./public/images');
+    },
+    filename: (req, file, cb) => {
+      cb(null,Date.now() + file.originalname);
+    }
+});
+const upload = multer({storage: storage});
 
 //var
 let userlogin = false;
@@ -57,6 +68,29 @@ app.post("/createuser", async (req,res) => {
     try{
         const response = await axios.post(url + "/createuser",req.body);
         res.redirect("/");
+    }
+    catch{
+        res.status(500).send("error");
+    }
+});
+
+app.get("/createcar",(req,res) => {
+    res.render("createcar.ejs");
+});
+
+app.post("/createcar",upload.single("Image"), async (req,res) => {
+    try{
+        let data = {
+            licenseplate:req.body.licenseplate,
+            year:req.body.year,
+            capacity:req.body.capacity,
+            priceperday:req.body.priceperday,
+            insurance_charge:req.body.insurance_charge,
+            brand:req.body.brand,
+            color:req.body.color,
+            Image:req.file.filename
+        };
+        const response = await axios.post(url + "/createcar",data);
     }
     catch{
         res.status(500).send("error");
